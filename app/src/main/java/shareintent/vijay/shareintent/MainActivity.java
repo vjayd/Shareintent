7f6898fb90458bf62bcec088dfe4bcaa4c98cade
vijay <vijay@perk.com>
2016-06-20 14:09:33 +0530

    Return result to activity

--- app/src/main/java/shareintent/vijay/shareintent/MainActivity.java
+++ app/src/main/java/shareintent/vijay/shareintent/MainActivity.java
@@ -1,16 +1,20 @@
 package shareintent.vijay.shareintent;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
+    static final int PICK_CONTACT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +64,47 @@ public class MainActivity extends AppCompatActivity {
     public void share(View view){
         Intent intent = new Intent(Intent.ACTION_SEND);
         String title = "Share It";
-// Create intent to show chooser
-        Intent chooser = Intent.createChooser(intent, title);
 
-// Verify the intent will resolve to at least one activity
+        Intent chooser = Intent.createChooser(intent, title);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(chooser);
         }
     }
 
+    public void pickcontact(View view){
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+
+    }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+                Toast.makeText(MainActivity.this,"Number : "+number,Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
 }
